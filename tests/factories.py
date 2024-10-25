68a614fbd82f6de36bb6ed2ba08ab537e0cee8cd
kachichou <ali.alidg55@gmail.com>
2024-10-25 10:32:35 -0400

    first commit

--- tests/factories.py
+++ tests/factories.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 # Copyright 2016, 2022 John J. Rofrano. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -17,18 +19,45 @@
 """
 Test Factory to make fake objects for testing
 """
+
 import factory
 from factory.fuzzy import FuzzyChoice, FuzzyDecimal
 from service.models import Product, Category
 
 
 class ProductFactory(factory.Factory):
+
     """Creates fake products for testing"""
 
     class Meta:
+
         """Maps factory to data model"""
 
-        model = Product
+    model = Product
 
     id = factory.Sequence(lambda n: n)
-   ## Add code to create Fake Products 
+    name = FuzzyChoice(choices=[
+        'Hat',
+        'Pants',
+        'Shirt',
+        'Apple',
+        'Banana',
+        'Pots',
+        'Towels',
+        'Ford',
+        'Chevy',
+        'Hammer',
+        'Wrench',
+        ])
+
+    description = factory.Faker('text')
+    price = FuzzyDecimal(0.5, 2000.0, 2)
+    available = FuzzyChoice(choices=[True, False])
+    category = FuzzyChoice(choices=[
+        Category.UNKNOWN,
+        Category.CLOTHS,
+        Category.FOOD,
+        Category.HOUSEWARES,
+        Category.AUTOMOTIVE,
+        Category.TOOLS,
+        ])
